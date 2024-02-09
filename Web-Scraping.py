0dc5a9be597ccfbe7e16b33ac24a1391c5472f18
Rachida2001 <123777131+Rachida2001@users.noreply.github.com>
2024-02-08 20:17:19 -0600

    Update Web-Scraping.py

--- Web-Scraping.py
+++ Web-Scraping.py
@@ -1,50 +1,40 @@
-#!/usr/bin/python3
 import requests
 import os
 from bs4 import BeautifulSoup
 
 URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
-HEADERS = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-}
+HEADERS = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
+
 page = requests.get(URL, headers=HEADERS)
 
 if page.status_code != 200:
     print("Unable to find URL")
 else:
     soup = BeautifulSoup(page.content, "html.parser")
-    movie_column = soup.find_all('tbody')
-    titles = soup.find_all('td', {'class' : 'titleColumn'})
-
+    titles = soup.find_all('div', {'class': 'ipc-title ipc-title--base ipc-title--title ipc-title-link-no-icon ipc-title--on-textPrimary sc-be6f1408-9 srahg cli-title'})
+    
     base_url = "https://www.imdb.com"
     for title in titles:
+        movie_title = title.a.text.split(".", 1)[1].strip()
         try:
-            # Get title only by pulling out the a tag and create a file name in a directory called Reviews
-            f = open("Reviews/" + title.a.text, 'w')
-        except FileNotFoundError:
-            # Directory not found, create Directory first
-            os.mkdir("Reviews")
-
-        movie_details_url = "{}{}".format(base_url, title.a['href'])
-        movie_page = requests.get(movie_details_url, headers=HEADERS)
-        soup = BeautifulSoup(movie_page.content, "html.parser")
-
-        # Find the User reviews href
-        ipc_link = soup.find_all('a', {'class' : 'ipc-link ipc-link--baseAlt ipc-link--inherit-color'})
-        for r in ipc_link:
-            if r.text == "User reviews":
-                review_link = r['href']
-
-        # Title sub needs to be stripped out to get FQDN of the Reviews page
-        title_sub = "/".join(title.a['href'].split('/', 3)[:3])
-        print(title_sub)
-
-        # Navigate to Movie title's review page
-        all_reviews_page = requests.get("{}{}/{}".format(base_url, title_sub, review_link))
-        soup = BeautifulSoup(all_reviews_page.content, "html.parser")
-        reviews = soup.find_all('div', {'class' : 'text show-more__control'})
-
-        for review in reviews:
-            comment = review.text
-            f.write(review.text + "\n")
-        f.close()
+            os.makedirs("Reviews", exist_ok=True)
+            with open("Reviews/{}.txt".format(movie_title), 'w', encoding='utf-8') as f:
+                movie_details_url = "{}{}".format(base_url, title.a['href'])
+                movie_page = requests.get(movie_details_url, headers=HEADERS)
+                soup = BeautifulSoup(movie_page.content, "html.parser")
+                
+                ipc_link = soup.find_all('a', {'class': 'ipc-link ipc-link--baseAlt ipc-link--inherit-color'})
+                for r in ipc_link:
+                    if r.text == "User reviews":
+                        review_link = r['href']
+                
+                all_reviews_page = requests.get("{}/{}".format(base_url, review_link))
+                soup = BeautifulSoup(all_reviews_page.content, "html.parser")
+                reviews = soup.find_all('div', {'class': 'text show-more__control'})
+                
+                print('Recording reviews for {}...'.format(movie_title))
+                for review in reviews:
+                    comment = review.text
+                    f.write(comment + "\n")
+        except Exception as e:
+            print("Error:", e)
